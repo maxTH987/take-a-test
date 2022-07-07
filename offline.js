7dee65e816a41e3a9e142eb878bf129aff58d8ed
maxTH987 <107599886+maxTH987@users.noreply.github.com>
2022-07-07 13:42:07 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1657176049,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/playyer-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/bg.png",
+		"images/enamy1-sheet0.png",
+		"images/enemy2-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/bg2.png",
+		"images/bg3.png",
+		"images/bg4.png",
+		"images/bg6.png",
+		"images/play-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
